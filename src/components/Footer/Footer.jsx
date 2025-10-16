a4ce0b0cee0e889ea9c5db1d486f66acc174188a
Zacharytesch <zachctesch_10@hotmail.com>
2025-10-15 23:00:14 -0500

    Styling project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__name">
+        Developed by Zachary Tesch
+        <h2 className="footer__date">2025</h2>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
